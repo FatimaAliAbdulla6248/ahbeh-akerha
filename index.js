58b322fc760234e88c557e51b56b7cb98951f551
FatimaAliAbdulla6248 <fatimaradhi99@gmail.com>
2022-10-16 10:43:57 +0300

    updated

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const PORT =3000
 const ejsLayouts = require('express-ejs-layouts');
-
+// midleware
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
+//routes
+//home
+app.get('/',(req,res)=>{
+    res.render('home.ejs')
+})
 
-app.listen(3000)
+
+// favorite animals
+app.get('/animals', (req, res)=>{
+    const faveAnimals = ['sand crab', 'corny joke dog', 'benedict the sea cucumber']
+    res.render('animals.ejs', {animals: faveAnimals})
+})
+
+// favorite food
+app.get('/food', (req, res)=>{
+    const favefood = ['Salmon','Avocado','sushi']
+    res.render('food.ejs', {food: favefood})
+})
+
+// favorite movies
+app.get('/movies', (req, res)=>{
+    const favemovies = ['action','drama']
+    res.render('movies.ejs', {movies: favemovies})
+})
+
+
+// favorite products
+app.get('/products', (req, res)=>{
+    const faveproducts = ['skin care','organic food']
+    res.render('products.ejs', {products: faveproducts})
+     
+})
+
+app.listen(PORT,()=>{
+    console.log(`You are running !`)
+})
